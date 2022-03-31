307b68362257e86a9d9da7cfb5e2bc204eb21a20
DomenicoGalileo88 <domenico.galileo@gmail.com>
2022-03-31 17:06:24 +0200

    mod snack 5

--- snack_5/main.js
+++ snack_5/main.js
@@ -6,18 +6,45 @@ Crea due array che hanno un numero di elementi diversi. Aggiungi elementi all’
 
 // Crea due array che hanno un numero di elementi diversi.
 
-let longArray = ['Domenico', 'Luca', 'Luigi', 'Federica', 'Luisa', 'Antonella'];
+let arrayUno = ['Domenico', 'Luca', 'Luigi', 'Federica', 'Luisa', 'Antonella'];
 
-let shortArray = ['Marco', 'Antonio', 'Alice', 'Martina'];
+let arrayDue = ['Marco', 'Antonio', 'Alice', 'Martina'];
+
+let short;
+let long;
 
 // Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
-let short = shortArray.length;
+if (arrayUno.length > arrayDue.length) {
+    short = arrayDue;
+    long = arrayUno;
+} else {
+    long = arrayDue;
+    short = arrayUno;
+}
 
-while (short < longArray.length){
+
+while (short.length < long.length){
+
+    short.push('Alessandro');
+    
+    short++;
+}
+
+console.log(short);
+
+
+
+
+
+
+/* let short = shortArray.length; */
+
+
+/* while (short < longArray.length){
 
     shortArray.push('Alessandro');
 
     short++;
 }
 
-console.log(shortArray);
+console.log(shortArray); */
