a81126e89be952b40ae8c4667afe0122cfa9995c
DomenicoGalileo88 <domenico.galileo@gmail.com>
2022-03-31 16:01:27 +0200

    Add end snack_4

--- snack_4/main.js
+++ snack_4/main.js
@@ -1,29 +1,25 @@
-/* SNACK 1 
-Il software deve chiedere per 5 volte all’utente di inserire un numero.
-Il programma stampa la somma di tutti i numeri inseriti.
-Esegui questo programma in due versioni, con il for e con il while. */
+/* 
+Snack4
+
+Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
+*/
+
+
+//Crea un array di numeri interi
+
+let numeri = [10, 20, 2, 6, 5, 7, 3];
+
+//fai la somma di tutti gli elementi che sono in posizione dispari
 
 let somma = 0;
 
-for (let i = 0; i < 5; i++) {
-   
-    let numero = parseInt(prompt('Inserisci un numero'));
-    somma += numero;    
+for (let i = 0; i < numeri.length; i++) {
+    const element = numeri[i];
+    
+    if (numeri.indexOf(element) % 2 !== 0) {
+        somma += element;
+    }
 }
 console.log(somma);
-
-console.log('Con for');
-
-/* let i = 0;
-
-while (i < 5) {
-
-    let numero = parseInt(prompt('Inserisci un numero'));
-    somma += numero;  
-
-    i++
-}
-
-console.log(somma); 
-console.log('Con while'); */
+let risultato = document.querySelector('.somma').innerHTML = `La somma dei numeri in posizione dispari dell'array è: ${somma}`
 
